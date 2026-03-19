8e73ef05ff6f0b6de8a66cfe334e2dc851f815eb
Mark Jivko <50949149+markjivko@users.noreply.github.com>
2026-03-19 11:22:10 +0200

    [chore] Improved performance

--- src/13-roman-to-integer.js
+++ src/13-roman-to-integer.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function (s) {
+const romanToInt = function (s) {
     const rValues = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
     const rDict = Object.keys(rValues).join("");
 
--- src/7-reverse-int.js
+++ src/7-reverse-int.js
@@ -1,8 +1,10 @@
+const MAX_PARTIALS = [];
+
 /**
  * @param {number} x
  * @return {number}
  */
-var reverse = function (x) {
+const reverse = function (x) {
     if (0 === x) {
         return x;
     }
@@ -11,21 +13,31 @@ var reverse = function (x) {
         return -1 * reverse(-x);
     }
 
-    // Maximum int (32-bit)
-    const MAX_INT = "2147483647";
+    // Integer length
     const length = Math.ceil(Math.log10(x + 1));
 
-    // Prepare the result
-    let partial = 0;
+    // Overflow checks
+    let ovfCheck = 10 === length;
+    let ovfPartial = 0;
+
+    // Initialize max partials
+    if (ovfCheck && !MAX_PARTIALS.length) {
+        for (let i = 0; i < 10; i++) {
+            MAX_PARTIALS[i] = parseInt("2147483647".substring(0, i + 1), 10);
+        }
+    }
+
+    // Final result
     let result = 0;
     let index = 0;
     while (x > 0) {
         const lastDigit = x % 10;
 
-        // 32-bit overflow
-        if (10 === length) {
-            partial = partial * 10 + lastDigit;
-            if (partial > parseInt(MAX_INT.substring(0, index + 1), 10)) {
+        if (ovfCheck) {
+            ovfPartial = ovfPartial * 10 + lastDigit;
+            if (ovfPartial < MAX_PARTIALS[index]) {
+                ovfCheck = false;
+            } else if (ovfPartial > MAX_PARTIALS[index]) {
                 result = 0;
                 break;
             }
